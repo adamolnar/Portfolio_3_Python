1222befb6e11d8f345fd8d4cdfb34f69f240b536
adamolnar <adamolnar121@gmail.com>
2023-07-14 18:41:15 +0000

    Add function to run after user choses either to play right away or get familiar with the rules

--- run.py
+++ run.py
@@ -5,11 +5,28 @@ import string
 
 
 print("Welcome to the Hangman Game!")
-print("Do you know the rules")
-print("(1)Yes, take me to the game.\n(2)No, show me the rules")
+print("Do you know the rules?")
+print("(1)Yes, take me to the game.\n(2)No, show me the rules.")
 
-user_choice = input("->")
-print(user_choice)
+user_choice = " "
+
+def user_decission(user_choice):
+    """
+    Function to determine action after user choice.
+    Either user beginnes the game without further informations, or he gets to know the rules.
+    If inncorect number is chosen it asks user to repet the action.
+    """
+    if user_choice == '1':
+	    print("This is where the fun begins!")
+        print("Loading game...")
+        load_words()
+    elif user_choice == '2':
+        print("You need to guess the word.")
+	    print("3 RULES")
+        print("1. You have six chances to guess the word.")
+        print("2. If You chose the letter it is delited from the list.")
+        print("3. You have 3 warnings. You lose one warning if you chose a letter which has been already used or symbols which are not letters  ")  
+    else:
 
 def load_words():
     """
@@ -25,7 +42,9 @@ def random_word(word_list):
     """
     return random.choice(word_list)
 
-test_word_list = ['sun', 'sky', 'water', 'fire', 'nature'] 
+test_word_list = ['hair', 'bug', 'water', 'rain', 'nature'] 
 print(random_word(test_word_list))
+
+
 
 
