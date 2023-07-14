d9bebde450b3dedb70afe6059a9005341cd16cef
adamolnar <adamolnar121@gmail.com>
2023-07-14 18:23:05 +0000

    Add function to create random word ans assigned it to the variable

--- run.py
+++ run.py
@@ -11,3 +11,21 @@ print("(1)Yes, take me to the game.\n(2)No, show me the rules")
 user_choice = input("->")
 print(user_choice)
 
+def load_words():
+    """
+    This function gets words from the library and inserts them in one virable
+    """
+    word_list =  open('words.txt', 'r').readline().split
+    return word_list
+
+def random_word(word_list):
+    """
+    Function to get random word from the word_list
+    Return random word and test it 
+    """
+    return random.choice(word_list)
+
+test_word_list = ['sun', 'sky', 'water', 'fire', 'nature'] 
+print(random_word(test_word_list))
+
+
