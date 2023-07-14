0d41263732f5d9566f4ef0d0c890e262a6092aad
adamolnar <adamolnar121@gmail.com>
2023-07-14 18:21:35 +0000

    Add random and string libraries and welcome text

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import string 
+
+
+
+
+print("Welcome to the Hangman Game!")
+print("Do you know the rules")
+print("(1)Yes, take me to the game.\n(2)No, show me the rules")
+
+user_choice = input("->")
+print(user_choice)
+
