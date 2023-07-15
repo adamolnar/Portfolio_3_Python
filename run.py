eac66ce2880e79c4ccc56d4cea063c435e903487
adamolnar <adamolnar121@gmail.com>
2023-07-15 15:48:04 +0000

    Add function to loop over user choice and either start the game or leave the game

--- run.py
+++ run.py
@@ -2,48 +2,55 @@ import random
 import string 
 
 
+
+print("Welcome to the Hangman Game!")
+print("Do you know the rules?")
+print("(1)Yes, take me to the game.\n(2)No, show me the rules.")
+
 def intro():
     """
     Function to determine action after user choice.
     Either user beginnes the game without further informations, or he gets to know the rules.
     If inncorect number is chosen it asks user to repet the action.
     """
-    print("Welcome to the Hangman Game!")
-    print("Do you know the rules?")
-    print("(1)Yes, take me to the game.\n(2)No, show me the rules.")
-    user_choice = input("-> ")
-        
-    if (user_choice == '1'):
-        print("This is where the fun begins!")
-        load_words()
-    elif (user_choice == '2'):
-        print("\n")
-        print("RULES")
-        print("-> You have six chances to guess the word.")
-        print("-> If You chose the letter, it will be delited from the list.")
-        print("-> You have 3 warnings: you lose one warning if you chose a letter which has been already used or symbols which are not letters  ")  
-    else:
-        print("Wrong choice, please enter your number again.")
+    user_choice1 = input("-> ")
+    for num in user_choice1:    
+        if (user_choice1 == '1'):
+            print("This is where the fun begins!")
+            load_words()
+        elif (user_choice1 == '2'):
+            print("\n")
+            print("RULES")
+            print("-> You have six chances to guess the word.")
+            print("-> If You chose the letter, it will be delited from the list.")
+            print("-> You have 3 warnings: you lose one warning if you chose a letter which has been already used or symbols which are not letters  ")
+            print("\n")
+            print("Are you ready now to begin the game?")
+            print("(1)Yes, take me to the game.\n(2)No, exit the game.")
+            wrong_choice()  
+        else:
+            print("Wrong choice, please enter your number again.")
         intro()
 
-                
+def wrong_choice(): 
+    """
+    Function to loop over user choice until he choses correct number to either start the game or exit the game
+    """
+    user_choice2 = input("-> ")
+    for num in user_choice2:
+        if (user_choice2 == '1'):
+            load_words()
+        elif (user_choice2 == '2'):
+            print('THE END')
+            exit()
+        else:
+            print("Wrong choice, please enter your number again.")
+        wrong_choice()
+            
 
-def load_words():
-    """
-    This function gets words from the library and inserts them in one variable
-    """
-    word_list =  open('words.txt', 'r').readline().split
-    return word_list
 
-def random_word(word_list):
-    """
-    Function to get random word from the word_list
-    Return random word and test it 
-    """
-    return random.choice(word_list)
+              
 
-test_word_list = ['hair', 'bug', 'water', 'rain', 'nature'] 
-print(random_word(test_word_list))
 
 
 start = intro()
